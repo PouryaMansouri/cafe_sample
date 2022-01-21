549e44ff3daaeb0b11e6998fb5cb1ad7de365b1f
Pourya <pourya.mansouri@hotmail.com>
2022-01-21 16:56:25 +0330

    Category model

--- menu_items/models.py
+++ menu_items/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from core.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(verbose_name='Name', max_length=250, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='parent', verbose_name='Parent Category')
+    ...
+
