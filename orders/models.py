4ae1df9e2a8aa0b422f41b0f7272e536be809b11
Pourya <pourya.mansouri@hotmail.com>
2022-01-21 17:17:04 +0330

    order item model

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from model_utils import Choices
 
 from core.models import BaseModel
+from menu_items.models import MenuItem
 
 ORDER_STATUS = Choices(
     (0, 'CANCEL', 'Cancel'),
@@ -22,5 +23,20 @@ class Order(BaseModel):
     def __str__(self):
         return f'Order {self.id} status :{self.status} '
 
+    @property
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        return sum(item.get_cost() for item in self.order_items.all())
+
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE, verbose_name='Order')
+    item = models.ForeignKey(MenuItem, related_name='menu_items', on_delete=models.CASCADE,
+                             verbose_name='Item')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Quantity')
+
+    def __str__(self):
+        return f"{self.quantity} * {self.item.name}"
+
+    @property
+    def get_cost(self):
+        return (self.item.price - self.item.discount) * self.quantity
