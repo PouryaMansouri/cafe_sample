2c22fd519ae65a008042bcd0a59e5a9738ba9594
Pourya <pourya.mansouri@hotmail.com>
2022-01-21 17:06:50 +0330

    add price,discount and status to MenuItem

--- menu_items/models.py
+++ menu_items/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from model_utils import Choices
 
 # Create your models here.
 from core.models import BaseModel
+
+ITEM_STATUS = Choices(
+    (0, 'AVAILABLE', 'Available for menu'),
+    (1, 'UNAVAILABLE', 'Unavailable for Menu'),
+)
 
 
 class Category(BaseModel):
@@ -13,4 +19,7 @@ class Category(BaseModel):
 class MenuItem(BaseModel):
     name = models.CharField(max_length=250, unique=True, verbose_name='Item name')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Category')
+    status = models.IntegerField(choices=ITEM_STATUS, default=ITEM_STATUS.AVAILABLE)
+    price = models.PositiveIntegerField(verbose_name='Price')
+    discount = models.PositiveIntegerField(verbose_name='Discount')
     ...
