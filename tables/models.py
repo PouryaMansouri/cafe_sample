f1613453ce0a1560acd4a238b3976878288dc739
Pourya <pourya.mansouri@hotmail.com>
2022-01-21 17:26:37 +0330

    table models

--- tables/models.py
+++ tables/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from core.models import BaseModel
+from model_utils import Choices
+
+TABLE_STATUS = Choices(
+    (0, 'EMPTY', 'Empty'),
+    (1, 'FULL', 'Full'),
+    (2, 'RESERVED', 'Reserved')
+)
+
+
+class Table(BaseModel):
+    status = models.IntegerField(choices=TABLE_STATUS, default=TABLE_STATUS.EMPTY)
+    capacity = models.PositiveIntegerField()
